d601012cd55664e349abe1104038bbb6a5591b16
Mordechai Weinstock <gefenemail@gmail.com>
2020-07-10 10:59:10 +0100

    refactored js code

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
 const buttons = document.querySelectorAll(".button"),
-      videos = document.querySelectorAll("video"),
-      icon = document.querySelector("i");
-
-window.addEventListener("keydown", (event) => {
-    const character = event.key,
-          button = document.querySelector(`div.${character}`),
-          sound = document.querySelector(`audio#${character}`),
-          video = document.querySelector(`div.${character} video`);
-
-    button.classList.add("transform-buttons");
-    icon.classList.add("transform-icon");
-    button.style.backgroundColor = "transparent";
-
-    sound.currentTime = 0;
-    sound.play();
-
-    video.currentTime = 0;
-    video.play()
-    
-    video.addEventListener("ended", () => {
-        button.style.backgroundColor = "#cd9bda";
-    });
-});
-
-buttons.forEach(button => {
-    button.addEventListener("click", (event) => {
-        const character = event.target.innerText.toLowerCase(),
-              button = document.querySelector(`div.${character}`),
-              sound = document.querySelector(`audio#${character}`),
-              video = document.querySelector(`div.${character} video`);
-
+    videos = document.querySelectorAll("video"),
+    icon = document.querySelector("i"),
+    func = character => {
+        const button = document.querySelector(`div.${character}`),
+            sound = document.querySelector(`audio#${character}`),
+            video = document.querySelector(`div.${character} video`);
+                
         button.classList.add("transform-buttons");
         icon.classList.add("transform-icon");
         button.style.backgroundColor = "transparent";
@@ -39,10 +15,21 @@ buttons.forEach(button => {
     
         video.currentTime = 0;
         video.play()
-
+        
         video.addEventListener("ended", () => {
             button.style.backgroundColor = "#cd9bda";
         });
+    };
+
+window.addEventListener("keydown", (event) => {
+    const character = event.key;
+    func(character);
+});
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const character = event.target.innerText.toLowerCase();
+        func(character);
     });
 
     button.addEventListener("transitionend", () => {
